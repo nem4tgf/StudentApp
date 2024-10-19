2c1e126e27cc6c7159e27ba9f6ac4dc28c2df5f3
nem4tgf <143236099+nem4tgf@users.noreply.github.com>
2024-10-19 08:54:51 +0700

    as

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,12 +1,9 @@
 namespace StudentApp.Models{
     public class Student{
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public int Age { get; set; }
-        public string Email { get; set; }
+        public required string Email { get; set; }
     }
-
-
-
 
 }
--- obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6617a95b7fa55270b2e8a49393217c7fe44238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3bda71b66f09c941bf54ee0a4cb059ca75780b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
